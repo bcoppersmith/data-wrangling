bb27ed8663c499b2ec951f617a0aef41eb9610e3
bcoppersmith <ben@factual.com>
2015-09-12 00:45:22 -0700

    Standardizing around double quotes

--- wikipedia/billboard/lib/helpers/wiki_billboard_helper.rb
+++ wikipedia/billboard/lib/helpers/wiki_billboard_helper.rb
@@ -4,7 +4,7 @@ module BillboardParser
     extend self
 
     def get_songs(doc)
-      rows = doc.css('div.mw-content-ltr table.wikitable tr')
+      rows = doc.css("div.mw-content-ltr table.wikitable tr")
       rows.map { |tr| parse_row(tr) }.compact
     end
 
@@ -12,8 +12,8 @@ module BillboardParser
       offset = is_a_date?(tr.css("td[1]").text) ? 1 : 0
 
       song = {}
-      song[:link]   = tr.css("td[#{1+offset}] a").map { |link| link['href'] if link['href'].match(/^\/wiki/)}.first
-      song[:artist] = tr.css("td[#{2+offset}]").text.gsub(/ featuring .*$/, '')
+      song[:link]   = tr.css("td[#{1+offset}] a").map { |link| link["href"] if link["href"].match(/^\/wiki/)}.first
+      song[:artist] = tr.css("td[#{2+offset}]").text.gsub(/ featuring .*$/, "")
 
       song unless song.any? { |key, value| value.nil? }
     end
